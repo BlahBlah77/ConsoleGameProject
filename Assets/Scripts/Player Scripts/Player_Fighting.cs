fc627c8d7ab430801ba61d611b670ad6b468f40e
BlahBlah77 <k1623759@kingston.ac.uk>
2019-03-04 16:12:12 +0000

    Beta Changes
    Animations work now left click attacks

--- Assets/Scripts/Player Scripts/Player_Fighting.cs
+++ Assets/Scripts/Player Scripts/Player_Fighting.cs
@@ -30,37 +30,43 @@ public class Player_Fighting : MonoBehaviour {
 
     void Comboer()
     {
-        //playerRefs.playerPCM.isAttacking = true;
         if(isClickable)
         {
             clickNum++;
         }
-        if(clickNum == 1)
+        if(clickNum == 1 && playerRefs.anim.GetInteger("AnimationInt") == 0)
         {
             playerRefs.anim.SetInteger("AnimationInt", 1);
         }
     }
 
+    public void ClickReset()
+    {
+        clickNum = 0;
+    }
+
     public void CheckComboStatus()
     {
         isClickable = false;
-        if(playerRefs.anim.GetInteger("AnimationInt") == 1 && clickNum == 1)
+        if(playerRefs.anim.GetInteger("AnimationInt") == 1 && clickNum == 0)
         {
             playerRefs.anim.SetInteger("AnimationInt", 0);
-            isClickable = true;
-            clickNum = 0;
         }
-        else if (playerRefs.anim.GetInteger("AnimationInt") == 1 && clickNum >= 2)
+        else if (playerRefs.anim.GetInteger("AnimationInt") == 1 && clickNum >= 1)
         {
             playerRefs.anim.SetInteger("AnimationInt", 2);
-            isClickable = true;
-            //clickNum = 0;
         }
-        else if (playerRefs.anim.GetInteger("AnimationInt") == 2 && clickNum >= 2)
+        else if (playerRefs.anim.GetInteger("AnimationInt") == 2 && clickNum == 0)
         {
             playerRefs.anim.SetInteger("AnimationInt", 0);
-            isClickable = true;
-            clickNum = 0;
+        }
+        else if (playerRefs.anim.GetInteger("AnimationInt") == 2 && clickNum >= 1)
+        {
+            playerRefs.anim.SetInteger("AnimationInt", 3);
+        }
+        else if (playerRefs.anim.GetInteger("AnimationInt") == 3 && clickNum >= 0)
+        {
+            playerRefs.anim.SetInteger("AnimationInt", 0);
         }
         isClickable = true;
     }
