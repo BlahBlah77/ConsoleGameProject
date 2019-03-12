23e208ee3157fa8c7ae6b64b19535699a0d3b3ac
BlahBlah77 <k1623759@kingston.ac.uk>
2019-03-12 21:38:29 +0000

    Changes

---
+++ Assets/Scripts/Gif_Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gif_Player : MonoBehaviour {
+
+    [SerializeField] private Texture2D[] frameArray;
+    [SerializeField] private float framesPerSecond = 12.0f;
+
+    private Material deunydd;
+
+    private void Start()
+    {
+        deunydd = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        int ind = (int)(Time.time * framesPerSecond);
+        ind = ind % frameArray.Length;
+        deunydd.mainTexture = frameArray[ind];
+    }
+}
