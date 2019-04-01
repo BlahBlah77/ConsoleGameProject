b03eb09f971a7858885550218e5a6d3fea60ab5f
BlahBlah77 <k1623759@kingston.ac.uk>
2019-04-01 22:31:50 +0100

    Level ender added

---
+++ Assets/Rotate_Complex.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Complex : MonoBehaviour {
+
+    public float amplificationFactor = 0.5f;
+    public float frequencyFactor = 1.0f;
+    public float degreesFactor = 15.0f;
+    public Vector3 rotationFactor = new Vector3(15, 30, 45);
+
+    public Vector3 posOffset;
+    public Vector3 posTemp;
+
+    private void Start()
+    {
+        posOffset = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(rotationFactor * Time.deltaTime);
+        posTemp = posOffset;
+        posTemp.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequencyFactor) * amplificationFactor;
+        transform.localPosition = posTemp;
+    }
+}
--- Assets/Scripts/UI Scripts/Menu_UI_Manager.cs
+++ Assets/Scripts/UI Scripts/Menu_UI_Manager.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu_UI_Manager : MonoBehaviour {
 
     public string startGameScene;
+    
     public Material menuMat;
+    public AudioSource audSource;
+    public Slider volumeSlider;
+
+    [Header("UI Panels")]
+    public RectTransform currentPanel;
+    public RectTransform mainPanel;
+    public RectTransform optionsPanel;
+
 
     private void Awake()
     {
+        audSource = GetComponent<AudioSource>();
         RenderSettings.skybox = menuMat;
+        currentPanel = mainPanel;
     }
 
     private void Start()
     {
+        volumeSlider.value = AudioListener.volume;
         Time.timeScale = 1.0f;
     }
 
@@ -26,6 +39,37 @@ public class Menu_UI_Manager : MonoBehaviour {
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void MainPanelEnable()
+    {
+        EnableUIPanel(mainPanel);
+    }
+
+    public void OptionPanelEnable()
+    {
+        EnableUIPanel(optionsPanel);
+    }
+
+    public void EnableUIPanel(RectTransform newPanel)
+    {
+        if (currentPanel != null)
+        {
+            currentPanel.gameObject.SetActive(false);
+        }
+        newPanel.gameObject.SetActive(true);
+        currentPanel = newPanel;
+    }
+
+    public void SaveOptions()
+    {
+        Game_Manager.Instance.OptionSave();
+    }
+
+    public void VolumeAdjustment(Slider slider)
+    {
+        //Takes slider value and applies it to volume
+        AudioListener.volume = slider.value;
     }
 
     IEnumerator LoadSceneAsynchronously(string newLevel)
